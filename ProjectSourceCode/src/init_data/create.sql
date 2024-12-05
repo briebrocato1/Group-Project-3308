8a1176ee3c082ddb2565c0e38d05d417089a48aa
Brie Brocato <brie@lightspeedag.com>
2024-12-04 18:41:05 -0700

    sql

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,1 +1,49 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  username VARCHAR(50) PRIMARY KEY,
+  email VARCHAR(60) NOT NULL,
+  password CHAR(60) NOT NULL
+);
 
+DROP TABLE IF EXISTS routes CASCADE;
+CREATE TABLE routes (
+    id SERIAL PRIMARY KEY,
+    routeName VARCHAR(255) NOT NULL,
+    grade VARCHAR(10),
+    safety VARCHAR(10),
+    sport BOOLEAN NOT NULL,
+    trad BOOLEAN NOT NULL,
+    toprope BOOLEAN NOT NULL,
+    boulder BOOLEAN NOT NULL,
+    snow BOOLEAN NOT NULL,
+    alpine BOOLEAN,
+    description VARCHAR(3000),
+    location VARCHAR(3000),
+    areaLongitude DECIMAL(9,6),
+    areaLatitude DECIMAL(9,6), 
+    areaName VARCHAR(255),
+    firstAscent VARCHAR(255),
+    rating VARCHAR(255),
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(50) NOT NULL,
+    text VARCHAR(2500) NOT NULL,
+    parentID INT,
+    FOREIGN KEY (parentID) REFERENCES messages(id)
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(50) NOT NULL,
+    body VARCHAR(2500) NOT NULL,
+    rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    route_id INT NOT NULL,
+    CONSTRAINT fk_route FOREIGN KEY (route_id) REFERENCES routes(id)
+);
--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,1 +1,11 @@
+INSERT INTO users (username,email,password) VALUES ('admin','boulderingbuffs@gmail.com','$2a$10$t2FV25XVFoM.wF/Ge80CiuCNMS9LDUPQhebbCFxfycu5e9Dxqy7vq');
+
+INSERT INTO messages (author, text, parentID) VALUES
+-- Top-level messages
+('Bouldering Buffs', 'Thank you for using our site! Make sure not to post any private information, and use common sense.', NULL), -- Message 1
+('Bouldering Buffs', 'Post here with your best local climbing spots!', NULL), -- Message 2
+('Bouldering Buffs', 'Post here to find climbing partners. Remember to not post personal info, and just meet up on the mountain!', NULL), -- Message 3
+
+
+('Ollie', 'Anyone going to be at the 1st Flatiron Sunday?', 3); -- Reply to message 1 (parentID = 1)
 
