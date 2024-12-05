1a34203510feb27982eb912e393241ee4b820963
Ollie Costello <ocostello@HannahMontana>
2024-12-04 17:53:02 -0700

    messages

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -2,11 +2,11 @@ INSERT INTO users (username,email,password) VALUES ('admin','boulderingbuffs@gma
 
 INSERT INTO messages (author, text, parentID) VALUES
 -- Top-level messages
-('Bouldering Buffs', 'Thank you for using our site! Make sure not to post any private information, and use common sense.', NULL), -- Message 1
-('Bouldering Buffs', 'Post here with your best local climbing spots!', NULL), -- Message 2
-('Bouldering Buffs', 'Post here to find climbing partners. Remember to not post personal info, and just meet up on the mountain!', NULL), -- Message 3
+(`Bouldering Buffs`, `Thank you for using our site! Make sure not to post any private information, and use common sense.`, NULL), -- Message 1
+(`Bouldering Buffs`, `Post here with your best local climbing spots!`, NULL), -- Message 2
+(`Bouldering Buffs`, `Post here to find climbing partners. Remember to not post personal info, and just meet up on the mountain!`, NULL), -- Message 3
 
--- Insert replies to Message 1 (Alice's message)
-('Ollie', 'Anyone going to be at the 1st Flatiron Sunday?', 1), -- Reply to message 1 (parentID = 1)
+
+(`Ollie`, `Anyone going to be at the 1st Flatiron Sunday?`, 3), -- Reply to message 1 (parentID = 1)
 
 
